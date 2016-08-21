06c1772048623846caac9f8b9e711c8f2a0a3691
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2016-08-21 15:29:48 +0200

    `Matrix rotation` Completed

--- matrixrotation/matrixrotation.hs
+++ matrixrotation/matrixrotation.hs
@@ -1,3 +1,6 @@
+import Control.Applicative
+import Control.Monad
+
 -- (nrows, ncolumns)
 type MatrixSize = (Int, Int)
 
@@ -21,6 +24,9 @@ crust (firstRow:rs) = let innerRows = init rs
                           lastRow = last rs
                       in firstRow ++ rightBorder ++ (reverse lastRow) ++ (reverse leftBorder)
 
+crustSize :: MatrixSize -> Int
+crustSize (n,m) = 2*(m+n-2)
+
 regular2ring :: Matrix a -> RingMatrix a
 regular2ring  []  = EmptyRing
 regular2ring [[]] = EmptyRing
@@ -36,17 +42,38 @@ flattenedRow _ EmptyRing = []
 flattenedRow r (RingMatrix (n,m) bark kernel)
   | r ==   0 && n > 0 = take m bark
   | r == n-1 && n == 2 = take m . reverse $ bark
-  | r == n-1 = take m . drop (m-2) . reverse $ bark
-  | otherwise = let barkLength = 2*(m+n-2)
+  | r == n-1 = take m . drop (n-2) . reverse $ bark
+  | otherwise = let barkLength = crustSize (n,m)
                     firstElem  = bark !! (barkLength-r)
                     lastElem   = bark !! (m-1+r)
                 in [firstElem] ++ (flattenedRow (r-1) kernel) ++ [lastElem]
 
 ring2regular :: RingMatrix a -> Matrix a
 ring2regular rm@(RingMatrix (n,_) _ _) = map (\r -> flattenedRow r rm) [0..(n-1)]
-  
 
+rotateRing :: Int -> RingMatrix a -> RingMatrix a
+rotateRing r (RingMatrix mSize mCrust mKernel) = RingMatrix mSize (rotate r mCrust) $ rotateRing r mKernel
+  where rotate :: Int -> [a] -> [a]
+        rotate n l = let (a,b) = splitAt (n `mod` (crustSize mSize)) l in b++a
+rotateRing r EmptyRing = EmptyRing
 
+rotateMatrix :: Int -> Matrix a -> Matrix a
+rotateMatrix r = ring2regular . (rotateRing r) . regular2ring
 
+readMatrix :: MatrixSize -> IO (Matrix Int)
+readMatrix (n,m) = replicateM n $ (map (read :: String -> Int)) <$> words <$> getLine
 
-m = map (\x -> [x..x+3]) [10,20..40]
+main = do
+  [[n,m,r]] <- readMatrix (1,3)
+  m <- readMatrix (n,m)
+  let res = rotateMatrix r m
+  sequence $ map (putStrLn . (map (\c -> if c == ',' then ' ' else c)) . tail . init . show) res
+
+--m = map (\x -> [x..x+3]) [10,20..40]
+--m1 =
+--  [
+--    [1,	2,  3, 4],
+--    [5,	6,  7, 8],
+--    [9,	10,11,12],
+--    [13,14,15,16]
+--  ]
